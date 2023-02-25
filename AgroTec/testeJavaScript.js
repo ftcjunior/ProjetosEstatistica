28f6f093d8d0a0595a53c99df3ddcddd67853bc7
ftcjunior <126026798+ftcjunior@users.noreply.github.com>
2023-02-25 08:40:51 -0300

    aula de switch para teste

---
+++ AgroTec/testeJavaScript.js
@@ -0,0 +1,19 @@
+
+
+//Teste de switch para utilizar no estudo de probabilidade
+const junior = 6
+let idade = 52
+switch(junior){
+    case 3:
+        console.log('valor teste é verdadeiro')
+    break;
+    case 5:
+        console.log(`${idade} é verdadeiro`)
+    break;
+
+    default:
+        console.log('valor pacrão do switch')
+    break;
+
+
+}
