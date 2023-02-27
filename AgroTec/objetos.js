6ff99977c5f8a99d248b338d258fb6310f0ddf2d
ftcjunior <126026798+ftcjunior@users.noreply.github.com>
2023-02-27 12:19:41 -0300

    junior

--- AgroTec/objetos.js
+++ AgroTec/objetos.js
@@ -5,7 +5,7 @@ const carro = {
     marca: 'volks',
     modelo: 'zigzag',
     ano: 1996,
-    placa: '586',
+    placa: 586,
     dobrar: function(numero){
         if(numero == this.placa)
      return 'Dobrar a esquerda'
@@ -15,3 +15,14 @@ const carro = {
 }
 
 console.log(carro.dobrar(889))
+
+switch(carro.placa){
+    case 234:
+        console.log('não está certo')
+        break;
+    case 586:
+        console.log('Esta é a plalca certa')
+        break;
+    default:
+        console.log('padrão')
+}
