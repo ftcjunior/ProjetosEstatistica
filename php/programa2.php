3aead8f79bbda62d7736c4d3fd5bc5d3259f361c
ftcjunior <126026798+ftcjunior@users.noreply.github.com>
2023-03-02 07:23:45 -0300

    ftcjunior local

--- php/programa2.php
+++ php/programa2.php
@@ -1,9 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>repositório do GitHub clonado</title>
+</head>
+<body>
 
-<?php
+    <?php
 
-class carro{
-    $modelo = 'volks';
-    $cor = "amarelo";
+    echo "Eu estou testando o servidor local para php";
+
     
-
-}
+    ?>
+</body>
+</html>
